ee21c866c8a1cfef41e0a1cba42a854d03e958b3
James Hawker <526rserbinenko@frhsd.com>
2026-05-28 15:40:49 +0000

    Made a 404 page, that automatically resizes itself

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ import { createApp, ref, computed, reactive } from './central.js'
 import { timeMachine, project } from './components.js';
 import { Fuse } from './central.js'
 
+function resizeMap() {
+    const container = document.querySelector("#controller404");
+    const image = document.querySelector("#img404");
+    const { innerWidth: Width, innerHeight: Height }  = window;
+    const wFactor = Width / image.offsetWidth;
+    const hFactor = Height / image.offsetHeight;
+    const xTransform = 1 * wFactor;
+    const yTransform = 1 * hFactor;
+    container.setAttribute("style", `transform: scaleX(${xTransform}) scaleY(${yTransform})`)
+}
 const App = createApp({
     setup() {
         const data = reactive({ json: null })
@@ -17,6 +27,10 @@ const App = createApp({
             .catch(e => {
                 throw new Error(e)
             })
+        if (window.location.href.includes("404.html")) {
+            window.addEventListener("DOMContentLoaded", resizeMap);
+            window.addEventListener("resize", resizeMap);
+        }
         return {
             data
         }
